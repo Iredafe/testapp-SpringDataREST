37e0324d48b71595cd652b7004b826cc5916da80
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-03-13 22:24:31 -0700

    defining method findAll() for fetching from db

--- src/main/java/com/dafe/spring/AppLogger/dao/UserLogDaoHibernateImplementation.java
+++ src/main/java/com/dafe/spring/AppLogger/dao/UserLogDaoHibernateImplementation.java
@@ -3,9 +3,11 @@ package com.dafe.spring.AppLogger.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.websocket.Session;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dafe.spring.AppLogger.entity.UserLog;
 
@@ -16,21 +18,27 @@ public class UserLogDaoHibernateImplementation implements UserLogDAO {
 	//define field for entity manager
 	private EntityManager entityManager;
 	
-	
-	
 	//set up constructor injection
 	@Autowired
-	public UserLogDaoHibernateImplementation(EntityManager theManger) {
+	public UserLogDaoHibernateImplementation(EntityManager theEntityManager) {
 	
-	
-	
+	entityManager= theEntityManager;
 	}
 	
-	
-	
 	@Override
+	@Transactional
 	public List<UserLog> findAll() {
 
+		//get the current hibernate session from entity manager
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		
+		//create a query
+		
+		//execute query and get result list
+		
+		//return the results
+		
 		
 		return null;
 	}
